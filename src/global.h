3ac424007aa0cc2a880d6b637a9522c18475e1b7
ego <ego@localhost>
2026-04-24 10:36:35 +0200

    <platformio.ini> Erweiterung auf drei Boards(nano, esp, mega)

--- src/global.h
+++ src/global.h
@@ -2,20 +2,15 @@
 #include <Arduino.h>
 
 // Konstantendefinition 
-#include <Arduino.h>
-
 #ifdef ENV_NANO
-	const uint8_t LEDPIN = 12;                                            // pin ersetzen
+	const uint8_t LED1_PIN = 12;                                            // Die Pins können je nach Board unterschiedlich sein
 #elif defined(ENV_ESP32)
-	const uint8_t LEDPIN = 13;
+	const uint8_t LED1_PIN = 13;
 #elif defined(ENV_MEGA)
-	const uint8_t LEDPIN = 14;
+	const uint8_t LED1_PIN = 14;
 #else
-	const uint8_t LEDPIN = 2;
+	const uint8_t LED1_PIN = 2;
 #endif
-
-
-
 
 // Typendefinition
 typedef enum {
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include "basis.hpp"
 
-Led led1(LEDPIN);
+Led led1(LED1_PIN);
 
 void setup() {
+	Serial.begin(115200);
+	delay(500);  																									// Warte auf Serial-Monitor
+	Serial.println("\nDouble gestartet.\n");
   led1.init();
 }
 
