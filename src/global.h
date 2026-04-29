3f8a940638746ed76eba88d5b05b475473c7151b
ego <ego@localhost>
2026-04-29 10:01:17 +0200

    <global.h> static-Konstanten + Klassenvorwärtsdeklaration

--- src/global.h
+++ src/global.h
@@ -3,13 +3,13 @@
 
 // Konstantendefinition 
 #ifdef ENV_NANO
-	const uint8_t LED1_PIN = 12;                                            // Die Pins können je nach Board unterschiedlich sein
+	static const uint8_t LED1_PIN = 12;                                            // Die Pins können je nach Board unterschiedlich sein
 #elif defined(ENV_ESP32)
-	const uint8_t LED1_PIN = 13;
+	static const uint8_t LED1_PIN = 13;
 #elif defined(ENV_MEGA)
-	const uint8_t LED1_PIN = 14;
+	static const uint8_t LED1_PIN = 14;
 #else
-	const uint8_t LED1_PIN = 2;
+	static const uint8_t LED1_PIN = 2;
 #endif
 
 // Typendefinition
@@ -17,3 +17,6 @@ typedef enum {
   OFF = 0,
   ON
 } status_t;
+
+// Klassenvorwärtsdeklaration
+class  Led;
